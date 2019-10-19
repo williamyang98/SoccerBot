5f50d8e65e4347cc8fcbfe1c80be0e13714efa3d
William Yang <williamyang2017@gmail.com>
2019-10-20 00:41:11 +1100

    Updated profiler

--- profiler.py
+++ profiler.py
@@ -5,25 +5,27 @@ from timeit import default_timer
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default="assets/model/model.h5")
-    parser.add_argument("--total-profiles", default=10)
+    parser.add_argument("--total-samples", type=int, default=10)
 
     args = parser.parse_args()
 
     from src.app import D3DScreenshot, MSSScreenshot
     bounding_box = (0, 0, 400, 500)
-    d3d_screenshotter = D3DScreenshot(bounding_box)
-    mss_screenshotter = MSSScreenshot(bounding_box)
+    d3d_screenshotter = D3DScreenshot()
+    mss_screenshotter = MSSScreenshot()
 
-    dt = profile_time(d3d_screenshotter.get_screen_shot, args.total_profiles)()
+    print("Taking {:d} samples".format(args.total_samples))
+
+    dt = profile_time(d3d_screenshotter.get_screen_shot, args.total_samples)(bounding_box)
     print("D3D took {:.02f}ms".format(dt*1000))
 
-    dt = profile_time(mss_screenshotter.get_screen_shot, args.total_profiles)()
+    dt = profile_time(mss_screenshotter.get_screen_shot, args.total_samples)(bounding_box)
     print("MSS took {:.02f}ms".format(dt*1000))
 
     from src.model import Model
     model = Model.load(args.model)
     test_image = np.full((1, 160, 227, 3), 125)/255
-    dt = profile_time(model.predict, args.total_profiles)(test_image)
+    dt = profile_time(model.predict, args.total_samples)(test_image)
     print("Model took {:.02f}ms".format(dt*1000))
 
 
