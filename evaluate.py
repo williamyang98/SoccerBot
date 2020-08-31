a0b86f36b2b09153bcb5ceb5675e01dbbaf2d886
FiendChain <formulating.response@gmail.com>
2020-09-01 06:03:11 +1000

    Added evaluation of models

---
+++ evaluate.py
@@ -0,0 +1,107 @@
+import argparse
+import numpy as np
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", default="assets/models/cnn_227_160_quantized.tflite")
+    parser.add_argument("--quantized", action="store_false")
+    # parser.add_argument("--model", default="assets/models/cnn_113_80.h5f")
+    # parser.add_argument("--quantized", action="store_true")
+    parser.add_argument("--large", action="store_true")
+
+    args = parser.parse_args()
+
+    if not args.quantized:
+        from load_model import load_from_filepath
+        model, (HEIGHT, WIDTH) = load_from_filepath(args.model, args.large) 
+    else:
+        from load_model import load_quantized_model_from_filepath
+        model = load_quantized_model_from_filepath(args.model)
+        input_shape = model.input_shape
+        HEIGHT, WIDTH, _ = input_shape
+
+    if args.quantized: 
+        print(f"loaded model {HEIGHT}x{WIDTH} (quantized={args.quantized})")
+    else:
+        print(f"loaded model {HEIGHT}x{WIDTH} (largs={args.large})")
+
+    import tensorflow as tf
+    from load_tf_records import get_test_dataset
+
+    record_filenames = sorted(tf.io.gfile.glob("./assets/data/records/images-*.tfrec"))
+
+    dataset = get_test_dataset(record_filenames, (HEIGHT, WIDTH))
+
+    BATCH_SIZE = 128
+    dataset = dataset.batch(BATCH_SIZE)
+
+    def detect_accuracy(y_true, y_pred, thresh=0.8):
+        true_cls = y_true[:,2]
+        pred_cls = y_pred[:,2]
+        pred_cls = tf.math.greater(pred_cls, thresh)
+        pred_cls = tf.cast(pred_cls, true_cls.dtype)
+        
+        abs_err = tf.math.abs(true_cls-pred_cls)
+        return 1-tf.math.reduce_mean(abs_err)
+
+    def position_accuracy(y_true, y_pred):
+        true_cls = y_true[:,2]
+        true_pos = y_true[:,:2]
+        pred_pos = y_pred[:,:2]
+        
+        abs_err = tf.math.abs(true_pos-pred_pos)
+        dist_sqr_err = tf.math.reduce_sum(tf.math.square(abs_err), axis=1)
+        dist_err = tf.math.sqrt(dist_sqr_err)
+        
+        # only consider when object is there
+        dist_err = tf.math.multiply(dist_err, true_cls)
+        net_err = tf.math.reduce_sum(dist_err)
+        total_objects = tf.math.reduce_sum(true_cls)
+        
+        mean_err = net_err / tf.math.maximum(total_objects, 1)
+        return 1-mean_err     
+
+    metrics = []
+
+    record_size = 10000
+    N = len(record_filenames)*record_size
+    total_batches = N // BATCH_SIZE
+    
+    for batch_id, (images, labels) in enumerate(dataset.take(total_batches)):
+        batch_metrics = []
+        for i in range(BATCH_SIZE):
+            image = images[i:i+1]
+            y_true = labels[i:i+1]
+        
+            y_pred = model.predict(image)
+            detect_acc = detect_accuracy(y_true, y_pred)
+            pos_acc = position_accuracy(y_true, y_pred)
+
+
+            batch_metrics.append((detect_acc, pos_acc))
+            metrics.append((detect_acc, pos_acc))
+
+        detect_acc = sum([m[0] for m in batch_metrics]) / len(batch_metrics)
+        pos_acc = sum([m[1] for m in batch_metrics]) / len(batch_metrics)
+        print(f"batch: {batch_id:3d}/{total_batches}, detect_accuracy: {detect_acc:.3f}, position_accuracy: {pos_acc:.3f}\r", end='')
+    
+    detect_acc = sum([m[0] for m in metrics]) / len(metrics)
+    pos_acc = sum([m[1] for m in metrics]) / len(metrics)
+
+    print()
+    print(">> Results summary")
+    print(f"detect_accuracy: {detect_acc:.2f} position_accuracy: {pos_acc:.2f}")
+
+if __name__ == '__main__':
+    main()
+
+
+    
+
+    
+
+
+
+
+
+
---
+++ load_tf_records.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+
+AUTO = tf.data.experimental.AUTOTUNE
+
+def decode_image(image_data, target_size):
+    image = tf.image.decode_jpeg(image_data, channels=3)
+    image = tf.cast(image, tf.float32) / 255.0  # convert image to floats in [0, 1] range
+    image = tf.image.resize(image, target_size)
+    image = tf.reverse(image, axis=[-1])
+    #image = tf.reshape(image, [*TARGET_SIZE, 3]) # explicit size needed for TPU
+    return image
+
+def read_labeled_tfrecord(example, target_size):
+    LABELED_TFREC_FORMAT = {
+        "image": tf.io.FixedLenFeature([], tf.string), 
+        "image_name": tf.io.FixedLenFeature([], tf.string), 
+        "x_center": tf.io.FixedLenFeature([], tf.float32),  
+        "y_center": tf.io.FixedLenFeature([], tf.float32),  
+        "confidence": tf.io.FixedLenFeature([], tf.float32),  
+    }
+    example = tf.io.parse_single_example(example, LABELED_TFREC_FORMAT)
+    image = decode_image(example['image'], target_size)
+    label = (example['x_center'], example['y_center'], example['confidence'])
+    label = [tf.cast(x, tf.float32) for x in label]
+    return image, label
+
+def read_dataset(filenames, target_size):
+    # automatically interleaves reads from multiple files
+    dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTO) 
+
+    opt = tf.data.Options()
+    opt.experimental_deterministic = False
+    dataset = dataset.with_options(opt) 
+
+    def wrapped_reader(example):
+        return read_labeled_tfrecord(example, target_size)
+
+    dataset = dataset.map(wrapped_reader, num_parallel_calls=AUTO)
+    
+    return dataset
+
+def get_train_dataset(filenames, target_size):
+    d = read_dataset(filenames, target_size)
+#     d = d.map(data_augment, num_parallel_calls=AUTO)
+    d = d.repeat()
+    d = d.shuffle(2048)
+    # d = d.batch(BATCH_SIZE)
+    d = d.prefetch(AUTO)
+    return d
+
+def get_test_dataset(filenames, target_size):
+    d = read_dataset(filenames, target_size)
+    # d = d.batch(BATCH_SIZE)
+    d = d.cache()
+    d = d.prefetch(AUTO)
+    return d
