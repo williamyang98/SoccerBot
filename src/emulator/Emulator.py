3787ccffcd0105f3d7d62ee64df968dd7923d4ec
FiendChain <formulating.response@gmail.com>
2020-09-02 01:27:33 +1000

    Added beams

--- src/emulator/Emulator.py
+++ src/emulator/Emulator.py
@@ -8,10 +8,11 @@ from .Ball import Ball
 from .EmoteManager import EmoteManager
 from .HighScoreCounter import HighScoreCounter
 from .ScoreCounter import ScoreCounter
-from .util import clip
+from .BeamManager import BeamManager
 
 from .ImageLoader import ImageLoader
 
+from .util import clip
 
 class Emulator:
     def __init__(self, assets_dir, window_size):
@@ -37,18 +38,49 @@ class Emulator:
         self.score_counter.pos = Vec2D(window_size.x//2, 65)
 
         self.emote_manager = EmoteManager(self.images)
+        self.beam_manager = BeamManager(window_size)
 
         self.ball_spawn = Vec2D(window_size.x//2, window_size.y-self.ball.radius-10)
         self.on_fail()
+
+
+    def update(self, dt):
+        self.emote_manager.update(dt)
+        self.beam_manager.update(dt)
+
+        if not self.playing:
+            return
+
+        ball = self.ball
+        C_drag = 0.01
+        A_accel = Vec2D(0, self.acceleration)
+        A_drag = -C_drag*ball.vel
+        A_net = A_accel+A_drag
+
+        ball.vel += A_net*dt
+        ball.pos += ball.vel*dt
+
+        self.check_collision()
+
+    def render(self, surface):
+        self.high_score_counter.render(surface)
+        self.score_counter.render(surface)
+        self.ball.render(surface)
+        self.emote_manager.render(surface)
+
+        self.beam_manager.render(surface)
     
     def on_fail(self):
         self.playing = False
         self.ball.pos = self.ball_spawn.copy()
+        self.ball.vel.set(0, 0)
         self.highscore = max(self.score, self.highscore)
 
         self.high_score_counter.score = self.highscore
         self.score_counter.set_state(self.highscore, self.playing)
         self.score = 0
+
+        self.beam_manager.reset()
 
     def on_click(self, x, y):
         mouse_pos = Vec2D(x, y)
@@ -65,6 +97,7 @@ class Emulator:
         self.score += 1
         self.score_counter.set_state(self.score, self.playing)
         self.bounce_ball(mouse_pos)
+        self.beam_manager.on_score(self.score)
     
     def bounce_ball(self, mouse_pos):
         ball = self.ball
@@ -97,30 +130,6 @@ class Emulator:
         else:
             self.emote_manager.create_success(emote_pos)
     
-    def update(self, dt):
-        self.emote_manager.update(dt)
-
-        if not self.playing:
-            return
-
-        ball = self.ball
-
-        C_drag = 0.01
-        A_accel = Vec2D(0, self.acceleration)
-        A_drag = -C_drag*ball.vel
-        A_net = A_accel+A_drag
-
-        ball.vel += A_net*dt
-        ball.pos += ball.vel*dt
-
-        self.check_collision()
-
-    def render(self, surface):
-        self.high_score_counter.render(surface)
-        self.score_counter.render(surface)
-        self.ball.render(surface)
-        self.emote_manager.render(surface)
-
     def check_collision(self):
         ball = self.ball
         if ball.pos.x-ball.radius < 0:
@@ -130,5 +139,5 @@ class Emulator:
             ball.pos.x = self.window_size.x-ball.radius
             ball.vel.x = -abs(ball.vel.x)
 
-        if ball.pos.y-ball.radius > self.window_size.y:
+        if ball.pos.y-ball.radius*5 > self.window_size.y:
             self.on_fail()
--- src/emulator/util.py
+++ src/emulator/util.py
@@ -1,3 +1,13 @@
+import math
+from .Vec2D import Vec2D
 
 def clip(v, _min, _max):
     return max(min(v, _max), _min)
+
+def point_rot(p, sin, cos=None):
+    if cos is None:
+        sin, cos = math.sin(sin), math.cos(sin)
+
+    x = cos*p.x - sin*p.y
+    y = sin*p.x + cos*p.y
+    return Vec2D(x, y)
