2414f1024febb7bb6c1ec2f223ccf9a9e0de3599
FiendChain <formulating.response@gmail.com>
2020-09-03 20:31:49 +1000

    Refactored app

--- soccer.py
+++ soccer.py
@@ -1,36 +1,163 @@
 import argparse
+import threading
+import cv2
+import time
+import numpy as np
+from pynput.keyboard import *
+
 from src.app import App, Predictor, MSSScreenshot
+from src.util import draw_bounding_box
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default="assets/models/cnn_227_160_quantized.tflite")
     parser.add_argument("--checkpoint", action="store_true")
-    # parser.add_argument("--model", default="assets/models/cnn_113_80.h5f")
-    # parser.add_argument("--quantized", action="store_true")
     parser.add_argument("--large", action="store_true")
-
-    parser.add_argument("--preview", action='store_true')
-    parser.add_argument("--debug", action="store_true")
-
     args = parser.parse_args()
 
     from load_model import load_any_model_filepath
     model, (HEIGHT, WIDTH) = load_any_model_filepath(args.model, not args.checkpoint, args.large)
 
     # screen box is (x, y, width, height)
-    app = App(args.debug, args.preview)
-    # app.bounding_box = (677, 289, 325, 500)
-    # app.bounding_box = (677, 289, 400, 550)
-    app.bounding_box = (677, 289, 322, 455)
+    bounding_box = (677, 289, 322, 455)
+    app = App()
+    app.bounding_box = bounding_box
+
+    callback = ImageCallback(*bounding_box[2:4])
+    app.image_callback = callback.on_image
 
     screenshotter = MSSScreenshot()
     # screenshotter = D3DScreenshot()
 
     predictor = Predictor(model, (HEIGHT, WIDTH))
-    predictor.acceleration = 5
+    predictor.acceleration = 2.5
 
+    def on_press(key):
+        if key == Key.f1:
+            print("[Resumed]")
+            app.is_paused = False
+        elif key == Key.f2:
+            print("[Paused]")
+            app.is_paused = True
+        elif key == Key.f3:
+            print("[Exit]")
+            app.stop()
+            # callback.stop_threads()
+        elif key == Key.f4:
+            callback.is_preview = not callback.is_preview
+            print(f"[Preview={callback.is_preview}]")
+        elif key == Key.f5:
+            app.show_debug = not app.show_debug
+            print("[Debug={0}]".format(app.show_debug))
+        elif key == Key.f6:
+            app.track_only = not app.track_only
+            print("[Track={0}]".format(app.track_only))
+        elif key == Key.f7:
+            callback.is_recording = not callback.is_recording
+            print(f"[Recording={callback.is_recording}]")
+    
+
+    input_listener = Listener(on_press=on_press)
+    input_listener.start()
+    display_controls()
+
+    callback.start_threads()
     app.start(predictor, screenshotter)
+    input_listener.stop()
+    callback.stop_threads()
 
+def display_controls():
+    print("== Controls ==")
+    print("F1 : Resume")
+    print("F2 : Pause")
+    print("F3 : Exit")
+    print("F4 : Preview key")
+    print("F5 : Debug key")
+    print("F6 : Track only")
+    print("F7 : Record")
+    print("==============")
 
+class ImageCallback:
+    def __init__(self, width, height):
+        self.frame = 0
+        self.width = width
+        self.height = height
+
+        self.image = None 
+        self.is_running = False
+        self.is_recording = False
+        self.is_preview = False
+
+        self.thread_record = threading.Thread(target=self.start_video_thread)
+        self.thread_preview = threading.Thread(target=self.start_preview_thread)
+    
+    def start_threads(self):
+        self.is_running = True
+        self.thread_record.start()
+        self.thread_preview.start()
+    
+    def stop_threads(self):
+        self.is_running = False
+        self.thread_record.join()
+        self.thread_preview.join()
+
+    def on_image(self, image, pred_bb, real_bb):
+        if pred_bb is not None:
+            image = draw_bounding_box(image, pred_bb)
+            image = draw_bounding_box(image, real_bb, (255, 0, 0))
+            # display
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        self.image = image
+        self.frame += 1
+    
+    def start_video_thread(self):
+        writer = cv2.VideoWriter(
+            "video.mp4",
+            cv2.VideoWriter_fourcc(*'DIVX'), 
+                30, 
+                (self.width, self.height))
+
+        last_frame = None
+        total_frames = 0
+
+        while self.is_running:
+            image = self.image
+            if not self.is_recording or image is None:
+                time.sleep(15e-3)
+                continue
+
+            if last_frame == self.frame:
+                time.sleep(2e-3)
+                continue
+            
+            last_frame = self.frame
+            total_frames += 1
+
+            writer.write(image)
+
+        writer.release()
+        print(f"Wrote {total_frames} frames")
+
+    def start_preview_thread(self):
+        window_name = "Preview"
+
+        last_frame = None
+
+        while self.is_running:
+            if not self.is_preview:
+                cv2.destroyWindow(window_name)
+                time.sleep(15e-3)
+                continue
+        
+            if last_frame == self.frame or self.image is None:
+                time.sleep(15e-3)
+                continue
+            
+            last_frame = self.frame
+            cv2.imshow(window_name, self.image)
+
+            if cv2.waitKey(30) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+            
 if __name__ == "__main__":
     main()
--- src/app/App.py
+++ src/app/App.py
@@ -10,43 +10,23 @@ from src.util import *
 from src.app import *
 
 class App:
-    def __init__(self, show_debug=False, show_preview=False):
+    def __init__(self):
         # pyautogui flags
         pyautogui.FAILSAFE = True
         pyautogui.PAUSE = False
 
         self.is_running = False
         self.is_paused = True
-        self.show_debug = show_debug
-        self.show_preview = show_preview
+        self.show_debug = False
         self.track_only = False
-        self.recording = False
-        self.bounding_box = (0, 0, 0, 0)
-        
-        self.resume_key = Key.f1
-        self.pause_key = Key.f2
-        self.exit_key = Key.f3
-        self.preview_key = Key.f4
-        self.debug_key = Key.f5
-        self.track_key = Key.f6
-        self.record_key = Key.f7
 
-        self.input_listener = Listener(on_press=self.on_press)
-        self.preview = None
-        self.preview_thread_lock = threading.RLock()
+        self.bounding_box = (0, 0, 0, 0)
+        self.image_callback = None
     
     def start(self, predictor, screenshotter):
         print("[Bot ready]")
-        self.input_listener.start()
         self.is_running = True
-        self.display_controls()
-
-        preview_thread = threading.Thread(target=self.start_preview_thread)
-
-        preview_thread.start()
         self.start_prediction_thread(predictor, screenshotter)
-
-        preview_thread.join()
 
     def start_prediction_thread(self, predictor, screenshotter):
         screen_x, screen_y, screen_width, screen_height = self.bounding_box
@@ -54,15 +34,13 @@ class App:
         last_y = 0
         last_frame_time = 0
         click_delay = 4e-3
-        preview_delay = 0
-
-        predictor.acceleration = 2.5
+        callback_delay = 0
 
         cooldown_duration = 0
         cooldown = 0
 
         while self.is_running:
-            if not self.show_preview and not self.recording and self.is_paused:
+            if not self.image_callback and self.is_paused:
                 continue
 
             start = default_timer()
@@ -70,7 +48,7 @@ class App:
             end = default_timer()
             screen_shot_delay = end-start
 
-            predictor.additional_delay = screen_shot_delay+click_delay+preview_delay
+            predictor.additional_delay = screen_shot_delay+click_delay+callback_delay
 
             start = default_timer()
 
@@ -115,54 +93,17 @@ class App:
                     # else:
                         # pyautogui.moveTo(x=x, y=y)
             
-            if self.show_preview or self.recording:
+            if self.image_callback is not None:
                 start = default_timer()
-                with self.preview_thread_lock:
-                    if prediction:
-                        self.preview = (image, detected_bounding_box, real_bounding_box)
-                    else:
-                        self.preview = (image, None, None)
+                if prediction:
+                    self.image_callback(image, detected_bounding_box, real_bounding_box)
+                else:
+                    self.image_callback(image, None, None)
                 end = default_timer()    
-                preview_delay = end-start
-
+                callback_delay = end-start
 
     def stop(self):
         self.is_running = False
-        self.input_listener.stop()
-
-    def display_controls(self):
-        print("== Controls ==")
-        print("F1 : Resume")
-        print("F2 : Pause")
-        print("F3 : Exit")
-        print("F4 : Preview key")
-        print("F5 : Debug key")
-        print("F6 : Track only")
-        print("F7 : Record")
-        print("==============")
-
-    def on_press(self, key):
-        if key == self.resume_key:
-            print("[Resumed]")
-            self.is_paused = False
-        elif key == self.pause_key:
-            print("[Paused]")
-            self.is_paused = True
-        elif key == self.exit_key:
-            print("[Exit]")
-            self.stop()
-        elif key == self.preview_key:
-            self.show_preview = not self.show_preview
-            print("[Preview={0}]".format(self.show_preview))
-        elif key == self.debug_key:
-            self.show_debug = not self.show_debug
-            print("[Debug={0}]".format(self.show_debug))
-        elif key == self.track_key:
-            self.track_only = not self.track_only
-            print("[Track={0}]".format(self.track_only))
-        elif key == self.record_key:
-            self.recording = not self.recording
-            print(f"[Recording={self.recording}]")
 
     def check_mouse_inside(self, screen_bounding_box, pos):
         screen_x, screen_y, screen_width, screen_height = screen_bounding_box
@@ -173,47 +114,4 @@ class App:
             return False
         return True
 
-    def start_preview_thread(self):
-        window_name = "Preview"
-        _, _, width, height = self.bounding_box
-        video_out = cv2.VideoWriter(
-            "video.mp4",
-            cv2.VideoWriter_fourcc(*'DIVX'), 30, (width, height))
-        total_frames = 0
-
-        while self.is_running:
-            if not self.show_preview:
-                cv2.destroyWindow(window_name)
-                time.sleep(30e-3)
-
-            if not self.show_preview and not self.recording:
-                continue
-
-            with self.preview_thread_lock:
-                preview = self.preview
-                self.preview = None
-
-            if preview is None:
-                continue
-            # construct preview 
-            image, detected_bounding_box, real_bounding_box = preview 
-
-            if detected_bounding_box is not None:
-                draw_bounding_box(image, detected_bounding_box)
-                draw_bounding_box(image, real_bounding_box, (255, 0, 0))
-
-            # display
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-            if self.show_preview:
-                cv2.imshow(window_name, image)
-
-                if cv2.waitKey(30) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-            
-            if self.recording:
-                video_out.write(image)
-                total_frames += 1
-        
-        video_out.release()
-        print(f"Wrote {total_frames} frames")
+    
